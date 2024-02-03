55e7a3e28163a8e9acc0a415b7a4c9bbb754f17b
Yevgeniy Davydov <yevgeniydavydov@Yevgeniys-MBP.fritz.box>
2024-02-03 15:16:41 +0100

    hopefully final changes:)

--- JS/script.js
+++ JS/script.js
@@ -4,13 +4,7 @@
 
 const currentMaxValue = 4589;
 
-// let reverseMaxValue = Number(new String(currentMaxValue).split("").reverse().join(""));
-
-let valueString = currentMaxValue.toString();
-let valueArray = valueString.split("");
-let reversedArray = valueArray.reverse();
-let newValue = reversedArray.join("");
-let reverseMaxValue = Number(newValue);
+let reverseMaxValue = Number(currentMaxValue.toString().split("").reverse().join("")); // 3 Вариант
 
 
 console.log(reverseMaxValue); 
@@ -20,14 +14,9 @@ console.log(typeof reverseMaxValue);
 
 const resultsArray = [1, 2, [3, [4]]];
 let productOfArray = 1;
-let flattedArray = resultsArray.flat(Infinity);
 
-for (let number of flattedArray) {
-    productOfArray *= number;
+for (let number of resultsArray.flat(Infinity)){
+    productOfArray *= number;   
 }
-
-// for (let number of resultsArray.flat(Infinity)){
-//     productOfArray *= number;   
-// }
 
 console.log(productOfArray);
